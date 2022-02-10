90d5643c31c42e444123589e3f106f0f201615e9
ztz1989 <tianzhu.zhang1989@gmail.com>
2022-02-10 11:02:11 +0000

    LogMine results

--- benchmark/FLP_exp.py
+++ benchmark/FLP_exp.py
@@ -2,9 +2,12 @@
 
 import sys
 sys.path.append('../')
-from logparser import FLP
+#from logparser import FLP
 import os
 #import pandas as pd
+from pprint import pprint
+#from RI_precision1 import *
+from logparser.FLP.parser import *
 
 input_dir = '../logs/' # The input directory of log file
 #output_dir = 'AEL_result/' # The output directory of parsing results
@@ -130,13 +133,13 @@ benchmark_settings = {
         'merge_percent' : 0.5
         },
 
-    #'Mac': {
-        #'log_file': 'Mac/Mac_2k.log',
-        #'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
-        #'regex': [r'([\w-]+\.){2,}[\w-]+'],
-        #'minEventCount': 2,
-        #'merge_percent' : 0.6
-        #}
+    'Mac': {
+        'log_file': 'Mac/Mac_2k.log',
+        'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
+        'regex': [r'([\w-]+\.){2,}[\w-]+'],
+        'minEventCount': 2,
+        'merge_percent' : 0.6
+        }
 }
 
 bechmark_result = []
@@ -150,23 +153,8 @@ for dataset, setting in benchmark_settings.iteritems():
 	indir = os.path.join(input_dir, dataset)
     	#indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
     	#log_file = os.path.basename(setting['log_file'])
+	parserPara = Para(path=indir, st=0.5, removeCol=[0],logName=log_file)
 
-    	parser = AEL.LogParser(log_format=setting['log_format'], indir=indir, outdir=output_dir,
-                             minEventCount=setting['minEventCount'], merge_percent=setting['merge_percent'], rex=setting['regex'], keep_para=False)
-    	parser.parse(log_file)
+        myParser = Simplelogparser(parserPara)
+        time=myParser.mainProcess()
 
-        '''
-    	F1_measure, accuracy = evaluator.evaluate(
-                           groundtruth=os.path.join(indir, log_file + '_structured.csv'),
-                           parsedresult=os.path.join(output_dir, log_file + '_structured.csv')
-                           )
-    	bechmark_result.append([dataset, F1_measure, accuracy])
-    	'''
-
-'''
-print('\n=== Overall evaluation results ===')
-df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'F1_measure', 'Accuracy'])
-df_result.set_index('Dataset', inplace=True)
-print(df_result)
-df_result.T.to_csv('AEL_bechmark_result.csv')
-'''
